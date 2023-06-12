1025789bb830546ae595d10ddb107eedd6cb842b
webb <w@spiderden.org>
2023-06-12 11:12:19 -0400

    Make ID a type alias to string, remove compatibility code for numeric IDs with old Mastodon instances.

--- compat.go
+++ compat.go
@@ -2,28 +2,10 @@ package masta
 
 import (
 	"encoding/json"
-	"fmt"
 	"strconv"
 )
 
-type ID string
-
-func (id *ID) UnmarshalJSON(data []byte) error {
-	if len(data) > 0 && data[0] == '"' && data[len(data)-1] == '"' {
-		var s string
-		if err := json.Unmarshal(data, &s); err != nil {
-			return err
-		}
-		*id = ID(s)
-		return nil
-	}
-	var n int64
-	if err := json.Unmarshal(data, &n); err != nil {
-		return err
-	}
-	*id = ID(fmt.Sprint(n))
-	return nil
-}
+type ID = string
 
 type Sbool bool
 
